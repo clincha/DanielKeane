61fcda71618f630623eb5226901d1be17acf7b12
aclinch <NvnJgDvyvtckB4Fhmj6QCCZlkZb2kd>
2020-02-05 13:38:42 +0000

    Network config complete and now DanielKeane is a service

--- src/main/java/com/DanielKeane/DanielKeaneApplication.java
+++ src/main/java/com/DanielKeane/DanielKeaneApplication.java
@@ -2,10 +2,17 @@ package com.DanielKeane;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DanielKeaneApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DanielKeaneApplication.class, args);
-    }
+public class DanielKeaneApplication extends SpringBootServletInitializer {
+  public static void main(String[] args) {
+    SpringApplication.run(DanielKeaneApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(DanielKeaneApplication.class);
+  }
 }
