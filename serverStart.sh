12662599b33e8c17b372d6f6d63578ef2449242d
aclinch <NvnJgDvyvtckB4Fhmj6QCCZlkZb2kd>
2020-03-03 11:30:07 +0000

    Live setup for gallery

--- serverStart.sh
+++ serverStart.sh
@@ -4,4 +4,4 @@ iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 
-java -jar /app/build/libs/DanielKeane-live.war
+java -jar /app/DanielKeane/build/libs/DanielKeane-live.war
