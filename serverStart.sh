06e2cc37a0e5a205db073688c8a85ff5de82606f
aclinch <NvnJgDvyvtckB4Fhmj6QCCZlkZb2kd>
2020-03-04 15:45:52 +0000

    Adsense added

--- serverStart.sh
+++ serverStart.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
-
-java -jar /app/DanielKeane/build/libs/DanielKeane-live.war
+java -jar /app/DanielKeane/build/libs/DanielKeane-live.war --spring.profiles.active=live
