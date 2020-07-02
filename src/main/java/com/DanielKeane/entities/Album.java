9e5d06e20a2c80225c7c989375a70600fecc6531
clincha <angus.clinch@gmail.com>
2020-07-02 11:44:07 +0100

    Items are all there style is not great

--- src/main/java/com/DanielKeane/entities/Album.java
+++ src/main/java/com/DanielKeane/entities/Album.java
@@ -3,63 +3,69 @@ package com.DanielKeane.entities;
 import java.time.LocalDate;
 
 public class Album {
-  private final String id;
-  private final String name;
-  private final String albumGroup;
-  private final int totalTracks;
-  private final LocalDate releaseDate;
+    private final String id;
+    private final String name;
+    private final String albumGroup;
+    private final int totalTracks;
+    private final LocalDate releaseDate;
+    private final String imageUrl;
 
-  public Album(String id, String name, String albumGroup, int totalTracks, LocalDate releaseDate) {
-    this.id = id;
-    this.name = name;
-    this.albumGroup = albumGroup;
-    this.totalTracks = totalTracks;
-    this.releaseDate = releaseDate;
-  }
+    public Album(String id, String name, String albumGroup, int totalTracks, LocalDate releaseDate, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.albumGroup = albumGroup;
+        this.totalTracks = totalTracks;
+        this.releaseDate = releaseDate;
+        this.imageUrl = imageUrl;
+    }
 
-  @Override
-  public String toString() {
-    return "Album{" +
-      "id='" + id + '\'' +
-      ", name='" + name + '\'' +
-      ", albumGroup='" + albumGroup + '\'' +
-      ", totalTracks=" + totalTracks +
-      ", releaseDate=" + releaseDate +
-      '}';
-  }
+    @Override
+    public String toString() {
+        return "Album{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", albumGroup='" + albumGroup + '\'' +
+                ", totalTracks=" + totalTracks +
+                ", releaseDate=" + releaseDate +
+                '}';
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof Album)) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Album)) return false;
 
-    Album album = (Album) o;
+        Album album = (Album) o;
 
-    return getId().equals(album.getId());
-  }
+        return getId().equals(album.getId());
+    }
 
-  @Override
-  public int hashCode() {
-    return getId().hashCode();
-  }
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
 
-  public String getId() {
-    return id;
-  }
+    public String getId() {
+        return id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getAlbumGroup() {
-    return albumGroup;
-  }
+    public String getAlbumGroup() {
+        return albumGroup;
+    }
 
-  public int getTotalTracks() {
-    return totalTracks;
-  }
+    public int getTotalTracks() {
+        return totalTracks;
+    }
 
-  public LocalDate getReleaseDate() {
-    return releaseDate;
-  }
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
 }
--- src/main/java/com/DanielKeane/service/ReleaseService.java
+++ src/main/java/com/DanielKeane/service/ReleaseService.java
@@ -22,60 +22,61 @@ import java.util.LinkedHashMap;
 @Service
 public class ReleaseService {
 
-  @Value("${spotify.connection.id}")
-  private String CLIENT_ID;
+    @Value("${spotify.connection.id}")
+    private String CLIENT_ID;
 
-  @Value("${spotify.connection.secret}")
-  private String CLIENT_SECRET;
+    @Value("${spotify.connection.secret}")
+    private String CLIENT_SECRET;
 
-  public ArrayList<Album> getReleases() throws IOException, ParseException {
-    HttpUriRequest request;
-    CloseableHttpResponse response;
-    LinkedHashMap<String, Object> responseJson;
+    public ArrayList<Album> getReleases() throws IOException, ParseException {
+        HttpUriRequest request;
+        CloseableHttpResponse response;
+        LinkedHashMap<String, Object> responseJson;
 
-    CloseableHttpClient client = HttpClientBuilder.create().build();
+        CloseableHttpClient client = HttpClientBuilder.create().build();
 
-    request = RequestBuilder.post()
-      .setUri("https://accounts.spotify.com/api/token")
-      .setHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
-      .setHeader(HttpHeaders.AUTHORIZATION, "Basic " + getEncodedClientDetails())
-      .addParameter("grant_type", "client_credentials")
-      .build();
+        request = RequestBuilder.post()
+                .setUri("https://accounts.spotify.com/api/token")
+                .setHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
+                .setHeader(HttpHeaders.AUTHORIZATION, "Basic " + getEncodedClientDetails())
+                .addParameter("grant_type", "client_credentials")
+                .build();
 
-    response = client.execute(request);
+        response = client.execute(request);
 
-    responseJson = new JSONParser(response.getEntity().getContent()).parseObject();
+        responseJson = new JSONParser(response.getEntity().getContent()).parseObject();
 
-    final String accessToken = responseJson.get("access_token").toString();
+        final String accessToken = responseJson.get("access_token").toString();
 
-    request = RequestBuilder.get()
-      .setUri("https://api.spotify.com/v1/artists/7GijLdxqYBxYpORf7W5ex1/albums")
-      .setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-      .build();
+        request = RequestBuilder.get()
+                .setUri("https://api.spotify.com/v1/artists/7GijLdxqYBxYpORf7W5ex1/albums")
+                .setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .build();
 
-    response = client.execute(request);
+        response = client.execute(request);
 
-    responseJson = new JSONParser(response.getEntity().getContent()).parseObject();
+        responseJson = new JSONParser(response.getEntity().getContent()).parseObject();
 
-    ArrayList<LinkedHashMap<String, Object>> albumsJson = (ArrayList<LinkedHashMap<String, Object>>) responseJson.get("items");
+        ArrayList<LinkedHashMap<String, Object>> albumsJson = (ArrayList<LinkedHashMap<String, Object>>) responseJson.get("items");
 
-    ArrayList<Album> albums = new ArrayList<>(albumsJson.size());
-
-    for (LinkedHashMap<String, Object> albumJson : albumsJson) {
-      Album album = new Album(
-        albumJson.get("id").toString(),
-        albumJson.get("name").toString(),
-        albumJson.get("album_group").toString(),
-        ((BigInteger) albumJson.get("total_tracks")).intValue(),
-        LocalDate.parse((CharSequence) albumJson.get("release_date"))
-      );
-      albums.add(album);
+        ArrayList<Album> albums = new ArrayList<>(albumsJson.size());
+        for (LinkedHashMap<String, Object> albumJson : albumsJson) {
+            ArrayList<LinkedHashMap<String, String>> albumImages = (ArrayList<LinkedHashMap<String, String>>) albumJson.get("images");
+            Album album = new Album(
+                    albumJson.get("id").toString(),
+                    albumJson.get("name").toString(),
+                    albumJson.get("album_group").toString(),
+                    ((BigInteger) albumJson.get("total_tracks")).intValue(),
+                    LocalDate.parse((CharSequence) albumJson.get("release_date")),
+                    albumImages.get(0).get("url")
+            );
+            albums.add(album);
+        }
+        albums.removeIf(album -> album.getAlbumGroup().equals("appears_on"));
+        return albums;
     }
-    albums.removeIf(album -> album.getAlbumGroup().equals("appears_on"));
-    return albums;
-  }
 
-  private String getEncodedClientDetails() {
-    return Base64.getEncoder().withoutPadding().encodeToString((CLIENT_ID + ":" + CLIENT_SECRET).getBytes());
-  }
+    private String getEncodedClientDetails() {
+        return Base64.getEncoder().withoutPadding().encodeToString((CLIENT_ID + ":" + CLIENT_SECRET).getBytes());
+    }
 }
